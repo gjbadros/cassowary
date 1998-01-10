477c98cf51f2d947f997b1603e4bde6771251b1c
gjb <>
1998-01-10 00:32:36 +0000

    Added

---
+++ c++/ClAbstractVariable.cc
@@ -0,0 +1,14 @@
+// $Id$
+//
+// Cassowary Incremental Constraint Solver
+// Original Smalltalk Implementation by Alan Borning
+// This C++ Implementation by Greg J. Badros, <gjb@cs.washington.edu>
+// http://www.cs.washington.edu/homes/gjb
+// (C) 1998, All Rights Reserved.
+//
+// ClAbstractVariable.cc
+
+#include "ClAbstractVariable.h"
+#include <assert.h>
+
+long ClAbstractVariable::iVariableNumber = 0;
