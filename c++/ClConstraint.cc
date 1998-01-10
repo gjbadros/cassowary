e729f3b3bd6268b853919da2bb355dc7e49529d1
gjb <>
1998-01-10 22:19:59 +0000

    Added -- need to give default implementation of pure-virtual fn printOn in .cc file

---
+++ c++/ClConstraint.cc
@@ -0,0 +1,21 @@
+// $Id$
+//
+// Cassowary Incremental Constraint Solver
+// Original Smalltalk Implementation by Alan Borning
+// This C++ Implementation by Greg J. Badros, <gjb@cs.washington.edu>
+// http://www.cs.washington.edu/homes/gjb
+// (C) 1998, All Rights Reserved.
+//
+// ClConstraint.cc
+
+
+#include "ClConstraint.h"
+
+ostream &
+ClConstraint::printOn(ostream &xo) const 
+{
+  // Note that the trailing "= 0)" or ">= 0)" is missing, as derived classes will
+  // print the right thing after calling this function
+  xo << strength() << " {" << weight() << "} (" << expression();
+  return xo;
+}
