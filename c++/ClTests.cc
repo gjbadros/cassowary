b420fe7a2ea9b3e5e3a00ba214a1bdaa62629f00
gjb <>
1998-01-22 06:34:41 +0000

    Added addDel test

--- c++/ClTests.cc
+++ c++/ClTests.cc
@@ -9,6 +9,12 @@
 // ClTests.cc
 
 #include "Cl.h"
+#include <stdlib.h>
+#include "timer.h"
+
+inline 
+double UniformRandom()
+{ return double(rand())/RAND_MAX; }
 
 bool
 addDelete1()
@@ -228,6 +234,123 @@ inconsistent2()
    }
 }
 
+bool
+addDel(int nCns = 900, int nVars = 900, int nResolves = 10000)
+{
+  Timer timer;
+  // FIXGJB: from where did .12 come?
+  static const double ineqProb = 0.12;
+  static const int maxVars = 3;
+
+  cout << "starting timing test. nCns = " << nCns
+       << ", nVars = " << nVars << ", nResolves = " << nResolves << endl;
+
+  timer.Start();
+  ClSimplexSolver solver;
+
+  ClVariable *rgpclv[nVars];
+  for (int i = 0; i < nVars; i++)
+    {
+    rgpclv[i] = new ClVariable(i,"x");
+    solver.addStay(*rgpclv[i]);
+    }
+
+  ClConstraint *rgpcns[nCns];
+  int nvs = 0;
+  int k;
+  int j;
+  double coeff;
+  for (j = 0; j < nCns; j++)
+    {
+    // number of variables in this constraint
+    nvs = int(UniformRandom()*maxVars) + 1;
+    ClLinearExpression expr = UniformRandom()*20.0 - 10.0;
+    for (k = 0; k < nvs; k++)
+       {
+       coeff = UniformRandom()*10 - 5;
+       expr.addExpression(*(rgpclv[int(UniformRandom()*nVars)]) * coeff);
+       }
+    if (UniformRandom() < ineqProb)
+       {
+       rgpcns[j] = new ClLinearInequality(expr);
+       }
+    else
+       {  
+       rgpcns[j] = new ClLinearEquation(expr);
+       }
+    }
+
+  cout << "done building data structures" << endl;
+  cout << "time = " << timer.ElapsedTime() << "\n" << endl;
+  timer.Start();
+  int cExceptions = 0;
+  for (j = 0; j < nCns; j++)
+    {
+    // add the constraint -- if it's incompatible, just ignore it
+    // FIXGJB: exceptions are extra expensive in C++, so this might not
+    // be particularly fair
+    try
+      {
+      solver.addConstraint(*(rgpcns[j]));
+      }
+    catch (ExCLRequiredFailure &error)
+      {
+      cExceptions++;
+      rgpcns[j] = NULL;
+      }
+    }
+  // FIXGJB end = Timer.now();
+  cout << "done adding constraints [" << cExceptions << " exceptions]" << endl;
+  cout << "time = " << timer.ElapsedTime() << "\n" << endl;
+  timer.Start();
+
+  int e1Index = int(UniformRandom()*nVars);
+  int e2Index = int(UniformRandom()*nVars);
+
+  ClEditConstraint edit1(*(rgpclv[e1Index]),clsStrong());
+  ClEditConstraint edit2(*(rgpclv[e2Index]),clsStrong());
+
+  solver
+    .addConstraint(edit1)
+    .addConstraint(edit2);
+
+  cout << "done creating edit constraints -- about to start resolves" << endl;
+  cout << "time = " << timer.ElapsedTime() << "\n" << endl;
+  timer.Start();
+
+  // FIXGJB start = Timer.now();
+  for (int m = 0; m < nResolves; m++)
+    {
+    // FIXGJB: this is slow, too!
+    vector<Number> vals;
+    vals.push_back(rgpclv[e1Index]->value() * 1.001);
+    vals.push_back(rgpclv[e2Index]->value() * 1.001);
+    solver.resolve(vals);
+    }
+
+  // cout << "run time: " <<
+
+  cout << "done resolves -- now removing constraints" << endl;
+  cout << "time = " << timer.ElapsedTime() << "\n" <<endl;
+  timer.Start();
+
+  for (j = 0; j < nCns; j++)
+    {
+    if (rgpcns[j])
+      {
+      solver.removeConstraint(*(rgpcns[j]));
+      }
+    }
+
+  // FIXGJB end = Timer.now();
+  // cout << "Total remove time: " 
+  //      << "remove time per cn"
+  cout << "done removing constraints and addDel timing test" << endl;
+  cout << "time = " << timer.ElapsedTime() << "\n" <<endl;
+  timer.Start();
+  return true;
+}
+
 
 int
 main( char **argv, int argc )
@@ -245,6 +368,7 @@ if (!fResult) cout << "Failed!" << endl;
   RUN_TEST(casso1);
   RUN_TEST(inconsistent1);
   RUN_TEST(inconsistent2);
+  RUN_TEST(addDel);
   
 #undef RUN_TEST
 
