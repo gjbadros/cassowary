d2beb1ab18ffdc12eb53084dd039f5a75c1dfe00
gjb <>
1998-02-07 23:32:44 +0000

    First compilable version

--- java/ClErrors.java
+++ java/ClErrors.java
@@ -15,148 +15,47 @@
 // ExCLError
 //
 
-class ExCLError extends exception
+class ExCLError extends Exception
 {
 
-/**
-* description
-* @return char *
-*/
-public
-/* @c2j++: "char * description()" replacement: char *  to String  */
-String description()
-{ return "(ExCLError) An error has occured in CL"; }
-
+  public String description()
+    { return "(ExCLError) An error has occured in CL"; }
+  
 }
-
-/*
- * class ExCLInternalError
- * 
- * This code has been generated using C2J++
- * C2J++ is based on Chris Laffra's C2J (laffra@ms.com)
- * Read general disclaimer distributed with C2J++ before using this code
- * For information about C2J++, send mail to Ilya_Tilevich@ibi.com
- */
 
 class ExCLInternalError extends ExCLError
 {
-
-/**
-* description
-* @return char *
-*/
-public
-/* @c2j++: "char * description()" replacement: char *  to String  */
-String description()
-{ return "(ExCLInternalError) An internal error has occurred"; }
+  public String description()
+    { return "(ExCLInternalError) An internal error has occurred"; }
 }
-
-/*
- * class ExCLTooDifficult
- * 
- * This code has been generated using C2J++
- * C2J++ is based on Chris Laffra's C2J (laffra@ms.com)
- * Read general disclaimer distributed with C2J++ before using this code
- * For information about C2J++, send mail to Ilya_Tilevich@ibi.com
- */
 
 class ExCLTooDifficult extends ExCLError
 {
-
-/**
-* description
-* @return char *
-*/
-public
-/* @c2j++: "char * description()" replacement: char *  to String  */
-String description()
-{ return "(ExCLTooDifficult) The constraints are too difficult to solve"; }
+  public String description()
+    { return "(ExCLTooDifficult) The constraints are too difficult to solve"; }
 }
-
-/*
- * class ExCLRequiredFailure
- * 
- * This code has been generated using C2J++
- * C2J++ is based on Chris Laffra's C2J (laffra@ms.com)
- * Read general disclaimer distributed with C2J++ before using this code
- * For information about C2J++, send mail to Ilya_Tilevich@ibi.com
- */
 
 class ExCLRequiredFailure extends ExCLError
 {
-
-/**
-* description
-* @return char *
-*/
-public
-/* @c2j++: "char * description()" replacement: char *  to String  */
-String description()
-{ return "(ExCLRequiredFailure) A required constraint cannot be satisfied"; }
+  public String description()
+    { return "(ExCLRequiredFailure) A required constraint cannot be satisfied"; }
 }
 
-/*
- * class ExCLNotEnoughtStays
- * 
- * This code has been generated using C2J++
- * C2J++ is based on Chris Laffra's C2J (laffra@ms.com)
- * Read general disclaimer distributed with C2J++ before using this code
- * For information about C2J++, send mail to Ilya_Tilevich@ibi.com
- */
 
 class ExCLNotEnoughtStays extends ExCLError
 {
-
-/**
-* description
-* @return char *
-*/
-public
-/* @c2j++: "char * description()" replacement: char *  to String  */
-String description()
-{ return "(ExCLNotEnoughtStays) There are not enough stays to give specific values to every variable"; }
+  public String description()
+    { return "(ExCLNotEnoughtStays) There are not enough stays to give specific values to every variable"; }
 }
-
-/*
- * class ExCLNonlinearExpression
- * 
- * This code has been generated using C2J++
- * C2J++ is based on Chris Laffra's C2J (laffra@ms.com)
- * Read general disclaimer distributed with C2J++ before using this code
- * For information about C2J++, send mail to Ilya_Tilevich@ibi.com
- */
 
 class ExCLNonlinearExpression extends ExCLError
 {
-
-/**
-* description
-* @return char *
-*/
-public
-/* @c2j++: "char * description()" replacement: char *  to String  */
-String description()
-{ return "(ExCLNonlinearExpression) The resulting expression would be nonlinear"; }
+  public String description()
+    { return "(ExCLNonlinearExpression) The resulting expression would be nonlinear"; }
 }
-
-/*
- * class ExCLConstraintNotFound
- * 
- * This code has been generated using C2J++
- * C2J++ is based on Chris Laffra's C2J (laffra@ms.com)
- * Read general disclaimer distributed with C2J++ before using this code
- * For information about C2J++, send mail to Ilya_Tilevich@ibi.com
- */
 
 class ExCLConstraintNotFound extends ExCLError
 {
-
-/**
-* description
-* @return char *
-*/
-public
-/* @c2j++: "char * description()" replacement: char *  to String  */
-String description()
-{ return "(ExCLConstraintNotFound) Tried to remove a constraint never added to the tableu"; }
+  public String description()
+    { return "(ExCLConstraintNotFound) Tried to remove a constraint never added to the tableu"; }
 }
