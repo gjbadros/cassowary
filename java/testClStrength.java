2213085b1be353836c3fbc4a045664c843b01fad
gjb <>
1998-02-08 20:33:28 +0000

    Convert to java, only partial version of C++ impl.

--- java/testClStrength.java
+++ java/testClStrength.java
@@ -14,51 +14,20 @@
 //
 // testClStrength.java
 
+class testClStrength {
+  public static void main(String[] args) {
+    ClSymbolicWeight clsw = new ClSymbolicWeight(0.0,1.0,0.0);
+    ClSymbolicWeight clsw2 = new ClSymbolicWeight(2.0,0.5,0.5);
+    ClSymbolicWeight clsw3 = new ClSymbolicWeight(2.0,0.5,0.5);
+    ClSymbolicWeight clsw4 = new ClSymbolicWeight(2.0,0.4,0.5);
 
-#include <string>
-#include "Cassowary.h"
-#include "ClStrength.h"
-#include "debug.h"
+    System.out.println(ClStrength.clsRequired);
+    System.out.println(clsw2.asDouble());
 
-int
-main( char **, int  )
-{
-  ClSymbolicWeight clsw(0.0,1.0,0.0);
-  ClSymbolicWeight clsw2(2.0,0.5,0.5);
-  ClSymbolicWeight clsw3(2.0,0.5,0.5);
-  ClSymbolicWeight clsw4(2.0,0.4,0.5);
-
-  cout << clsw2.asDouble() << endl;
-
-  ClStrength cls(string("cls"),clsw);
-  cout << cls << endl;
-
-  ClStrength cls2(string("cls2"),clsw.times(2));
-  cout << cls2 << endl;
-
-  ClStrength cls3(string("cls3"),clsw.add(clsw2));
-  cout << cls3 << endl;
-
-  ClStrength cls4(string("cls4"),clsw.subtract(clsw2));
-  cout << cls4 << endl;
-
-  ClStrength cls5(string("cls5"),clsw.divideBy(2));
-  cout << cls5 << endl;
-
-  ClStrength a(clsRequired());
-  cout << "a is " << a.isRequired() << endl;
-
-  cout << clsw << (clsw == clsw2? "==" : "!=") << clsw2 << endl;
-  cout << clsw2 << (clsw2 == clsw3? "==" : "!=") << clsw3 << endl;
-  cout << clsw3 << (clsw3 == clsw4? "==" : "!=") << clsw4 << endl;
-
-  cout << clsw << (clsw < clsw2? "<" : "!<") << clsw2 << endl;
-  cout << clsw2 << (clsw2 < clsw3? "<" : "!<") << clsw3 << endl;
-  cout << clsw3 << (clsw3 < clsw4? "<" : "!<") << clsw4 << endl;
-
-  cout << clsw << (clsw > clsw2? ">" : "!>") << clsw2 << endl;
-  cout << clsw2 << (clsw2 > clsw3? ">" : "!>") << clsw3 << endl;
-  cout << clsw3 << (clsw3 > clsw4? ">" : "!>") << clsw4 << endl;
-
-  return 0;
+    ClStrength cls = new ClStrength("cls",clsw);
+    System.out.println(cls);
+    
+    ClStrength cls2 = new ClStrength("cls2", clsw.times(2));
+    System.out.println(cls2);
+  }
 }
