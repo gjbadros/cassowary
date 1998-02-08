a9d114c2128cb2f2be06df03df9faa448ce0c3c3
gjb <>
1998-02-08 03:19:52 +0000

    Added -- compiles

---
+++ java/Set.java
@@ -0,0 +1,51 @@
+// $Id$
+//
+// Cassowary Incremental Constraint Solver
+// Original Smalltalk Implementation by Alan Borning
+// This Java Implementation by Greg J. Badros, <gjb@cs.washington.edu>
+// http://www.cs.washington.edu/homes/gjb
+// (C) 1998, All Rights Reserved.
+//
+// (c) 1998 Alan Borning and Greg Badros.  This code is provided for use by
+// students for course projects in the course CSE 595 in the Department of
+// Computer Science and Engineering, University of Washington, during winter
+// quarter 1998.  Any other use requires written permission from the copyright
+// holders.
+//
+// Set
+
+import java.util.*;
+
+class Set {
+  public Set()
+    { hash = new Hashtable(); }
+
+  public Set(int i)
+    { hash = new Hashtable(i); }
+
+  public Set(int i, float f)
+    { hash = new Hashtable(i,f); }
+
+  public Set(Hashtable h) 
+    { hash = h; }
+
+  public boolean containsKey(Object o)
+    { return hash.containsKey(o); }
+
+  public boolean insert(Object o)
+    { return hash.put(o,o) == null? true: false; }
+
+  public boolean remove(Object o)
+    { return hash.remove(o) == null? true: false; }
+
+  public void clear()
+    { hash.clear(); }
+
+  public Object clone()
+    { return new Set((Hashtable) hash.clone()); }
+  
+  private Hashtable hash;
+}
+
+      
+
