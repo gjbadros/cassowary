e3d7a90a5fa355f117cc4cf19c6d2b124242e4b5
gjb <>
1998-02-09 07:10:18 +0000

    Added

---
+++ java/ExCLError.java
@@ -0,0 +1,24 @@
+// $Id$
+//
+// Cassowary Incremental Constraint Solver
+// Original Smalltalk Implementation by Alan Borning
+// This Java Implementation by Greg J. Badros, <gjb@cs.washington.edu>
+// http://www.cs.washington.edu/homes/gjb
+// (C) 1998, All Rights Reserved.
+//
+// (c) 1998 Alan Borning and Greg Badros.  This code is provided for use by
+// students for course projects in the course CSE 595 in the Department of
+// Computer Science and Engineering, University of Washington, during winter
+// quarter 1998.  Any other use requires written permission from the copyright
+// holders.
+//
+// ExCLError
+//
+
+class ExCLError extends Exception
+{
+
+  public String description()
+    { return "(ExCLError) An error has occured in CL"; }
+  
+}
