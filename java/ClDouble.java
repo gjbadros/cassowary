94627489ef7bec785b818adbcf74b8263dfe02d9
gjb <>
1998-02-11 21:47:20 +0000

    Added

---
+++ java/ClDouble.java
@@ -0,0 +1,51 @@
+// $Id$
+//
+// Cassowary Incremental Constraint Solver
+// Original Smalltalk Implementation by Alan Borning
+// This Java Implementation by Greg J. Badros, <gjb@cs.washington.edu>
+// http://www.cs.washington.edu/homes/gjb
+// (C) 1998, All Rights Reserved.
+//
+// (c) 1998 Alan Borning and Greg Badros.  This code is provided for use by
+// students for course projects in the course CSE 595 in the Department of
+// Computer Science and Engineering, University of Washington, during winter
+// quarter 1998.  Any other use requires written permission from the copyright
+// holders.
+//
+// ClDouble
+//
+
+class ClDouble extends Number
+{
+  public ClDouble(double val)
+    { value = val; }
+
+  public ClDouble()
+    { this(0.0); }
+
+  public final double doubleValue()
+    { return value; }
+
+  public final int intValue()
+    { return (int) value; }
+
+  public final long longValue()
+    { return (long) value; }
+
+  public final float floatValue()
+    { return (float) value; }
+
+  public final byte byteValue()
+    { return (byte) value; }
+
+  public final short shortValue()
+    { return (short) value; }
+
+  public final void setValue(double val)
+    { value = val; }
+
+  public final String toString()
+    { return java.lang.Double.toString(value); }
+
+  private double value;
+}
