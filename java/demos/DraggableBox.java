49ffd5a6435ddf3b4cd7f193edf00c5632f881f1
gjb <>
1998-02-14 08:55:50 +0000

    Added a debug-ctr w/ variable name (actually, number) argument Removed ^M's from whole file Added initial comment

--- java/demos/DraggableBox.java
+++ java/demos/DraggableBox.java
@@ -1,10 +1,22 @@
-/*
- * Draggable box class
- *
- * Michael Noth
- * 9 February 1998
- *
- */
+// $Id$
+//
+// Cassowary Incremental Constraint Solver
+// Original Smalltalk Implementation by Alan Borning
+// This C++ Implementation by Greg J. Badros, <gjb@cs.washington.edu>
+// http://www.cs.washington.edu/homes/gjb
+// (C) 1998, All Rights Reserved.
+//
+// (c) 1998 Alan Borning, Michael Noth, and Greg Badros.  
+// This code is provided for use by students for course projects in
+// the course CSE 595 in the Department of Computer Science and
+// Engineering, University of Washington, during winter quarter 1998.
+// Any other use requires written permission from the copyright
+// holders.
+//
+// Implementation of the DraggableBox class
+// By Michael Noth, 9 Feb 1998
+//
+// DraggableBox.java
 
 import java.awt.Graphics;
 
@@ -17,6 +29,15 @@ public class DraggableBox {
     center = new ClPoint(x, y);
     this.width = width;
     this.height = height;
+    cvt();
+  }
+
+  // this version for debugging
+  // creates variables with names xa, ya
+  public DraggableBox(int a) {
+    center = new ClPoint(0, 0, a);
+    this.width = 6;
+    this.height = 6;
     cvt();
   }
 
