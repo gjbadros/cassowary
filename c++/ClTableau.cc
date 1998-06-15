0af1e71f90e7d41dab25dd1b3f46dc6a294192ce
gjb <>
1998-06-15 22:45:43 +0000

    Updated the copyright notice, removed cse595 restriction

--- c++/ClTableau.cc
+++ c++/ClTableau.cc
@@ -4,13 +4,8 @@
 // Original Smalltalk Implementation by Alan Borning
 // This C++ Implementation by Greg J. Badros, <gjb@cs.washington.edu>
 // http://www.cs.washington.edu/homes/gjb
-// (C) 1998, All Rights Reserved.
-//
-// (c) 1998 Alan Borning and Greg Badros.  This code is provided for use by
-// students for course projects in the course CSE 595 in the Department of
-// Computer Science and Engineering, University of Washington, during winter
-// quarter 1998.  Any other use requires written permission from the copyright
-// holders.
+// (C) 1998 Alan Borning and Greg Badros
+// See ../COPYRIGHT for legal details regarding this software
 //
 // ClTableau.cc
 
