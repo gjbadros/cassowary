de9e5c5f0a178014307690619545978880a6e259
gjb <>
1998-07-09 22:07:10 +0000

    Added test cases from Steve Wolfman's SAT project that cause problems -- some normal test cases commented out temporarily.

--- c++/ClTests.cc
+++ c++/ClTests.cc
@@ -12,6 +12,8 @@
 #include "Cl.h"
 #include <stdlib.h>
 #include "timer.h"
+#include <iostream>
+#include <iomanip>
 
 inline 
 double UniformRandom()
@@ -212,11 +214,13 @@ inconsistent1()
 {
   ClSimplexSolver solver;
   ClVariable x("x");
+  ClLinearEquation eq1(x,10.0);
+  ClLinearEquation eq2(x,5.0);
   try 
     {
     
-    solver.addConstraint( ClLinearEquation(x,10.0) );
-    solver.addConstraint( ClLinearEquation(x, 5.0) );
+    solver.addConstraint( eq1 );
+    solver.addConstraint( eq2 );
     
     // no exception, we failed!
     return(false);
@@ -225,6 +229,7 @@ inconsistent1()
     {
     // we want this exception to get thrown
     cout << "Success -- got the exception" << endl;
+    // solver.removeConstraint(eq2); this would throw a constraint not found exception
     //    cout << solver << endl;
     return(true);
     }
@@ -266,6 +271,207 @@ inconsistent2()
    {
    cerr << "Exception! " << error.description() << endl;
    return(false);
+   }
+ catch (...) 
+   {
+   cerr << "Unknown exception" << endl;
+   return(false);
+   }
+}
+
+bool
+inconsistent3()
+{
+  try
+    {
+    ClSimplexSolver solver;
+
+    ClVariable r1("v2");
+    ClVariable r2("v3");
+    ClVariable r3("v4");
+    ClVariable r4("v5");
+    ClVariable r5("v6");
+    ClVariable r6("v7");
+    ClVariable r7("v8");
+    ClVariable r8("v9");
+
+#if REAL_NAMES
+    ClVariable r1("r1");
+    ClVariable r2("r2");
+    ClVariable r3("r3");
+    ClVariable r4("r4");
+    ClVariable r5("r5");
+    ClVariable r6("r6");
+    ClVariable r7("r7");
+    ClVariable r8("r8");
+#endif
+
+    ClLinearEquation eq1(r1,60);
+    ClLinearEquation eq2(r2,30);
+    ClLinearEquation eq12(r3,2.5);
+    ClLinearEquation eq13(r6,0);
+    ClLinearInequality eq14(r5, cnGEQ, 0);
+    ClLinearInequality eq15(r8, cnLEQ, 2.5);
+    ClLinearInequality eq16(r7, cnGEQ, r6);
+    ClLinearInequality eq17(r8, cnGEQ, r7);
+    ClLinearEquation eq18(r4, r3 - r2/60.0);
+    ClLinearEquation eq19(r5, r4 - r1/60.0);
+    ClLinearInequality eq20(r4, cnGEQ, 0);
+    ClLinearInequality eq21(r5, cnGEQ, 0);
+    ClLinearEquation eq22(r7, r6 + r2/20.0);
+    ClLinearEquation eq23(r8, r7 + r1/20.0);
+    ClLinearEquation eq24(r4, r3 - r2/30.0);
+    ClLinearEquation eq25(r5, r4 - r1/30.0);
+    ClLinearInequality eq26(r4, cnGEQ, 0);
+    ClLinearInequality eq27(r5, cnGEQ, 0);
+    ClLinearEquation eq28(r7, r6 + r2/60.0);
+    ClLinearEquation eq29(r8, r7 + r1/60.0);
+
+#if 0
+    cout << "eq1:  " << solver.addConstraintNoException(eq1) << endl;
+    cout << "eq2:  " << solver.addConstraintNoException(eq2) << endl;
+    cout << "eq12: " << solver.addConstraintNoException(eq12) << endl;
+    cout << "eq13: " << solver.addConstraintNoException(eq13) << endl;
+    cout << "eq14: " << solver.addConstraintNoException(eq14) << endl;
+    cout << "eq15: " << solver.addConstraintNoException(eq15) << endl;
+    cout << "eq16: " << solver.addConstraintNoException(eq16) << endl;
+    cout << "eq17: " << solver.addConstraintNoException(eq17) << endl;
+    cout << "eq18: " << solver.addConstraintNoException(eq18) << endl;
+    cout << "eq19: " << solver.addConstraintNoException(eq19) << endl;
+    cout << "eq20: " << solver.addConstraintNoException(eq20) << endl;
+    cout << "eq21: " << solver.addConstraintNoException(eq21) << endl;
+    cout << "eq22: " << solver.addConstraintNoException(eq22) << endl;
+    cout << "eq23: " << solver.addConstraintNoException(eq23) << endl;
+    cout << "eq24: " << solver.addConstraintNoException(eq24) << endl;
+    cout << "eq25: " << solver.addConstraintNoException(eq25) << endl;
+    cout << "eq26: " << solver.addConstraintNoException(eq26) << endl;
+    cout << "eq27: " << solver.addConstraintNoException(eq27) << endl;
+    cout << "eq28: " << solver.addConstraintNoException(eq28) << endl;
+    cout << "eq29: " << solver.addConstraintNoException(eq29) << endl;
+#endif
+#if 0
+    solver.setAutosolve(false);
+
+    cout << "eq1:  " << solver.addConstraintNoException(eq1) << "\t" << eq1 << endl;
+    cerr << solver << endl;
+    cout << "eq29: " << solver.addConstraintNoException(eq29) << "\t" << eq29 << endl;
+    cerr << solver << endl;
+    cout << "eq24: " << solver.addConstraintNoException(eq24) << "\t" << eq24 << endl;
+    cerr << solver << endl;
+    cout << "eq17: " << solver.addConstraintNoException(eq17) << "\t" << eq17 << endl;
+    cerr << solver << endl;
+    cout << "eq16: " << solver.addConstraintNoException(eq16) << "\t" << eq16 << endl;
+    cerr << solver << endl;
+    cout << "eq15: " << solver.addConstraintNoException(eq15) << "\t" << eq15 << endl;
+    cerr << solver << endl;
+    cout << "eq14: " << solver.addConstraintNoException(eq14) << "\t" << eq14 << endl;
+    cerr << solver << endl;
+    cout << "eq13: " << solver.addConstraintNoException(eq13) << "\t" << eq13 << endl;
+    cerr << solver << endl;
+    cout << "eq12: " << solver.addConstraintNoException(eq12) << "\t" << eq12 << endl;
+    cerr << solver << endl;
+    cout << "eq26: " << solver.addConstraintNoException(eq26) << "\t" << eq26 << endl;
+    cerr << solver << endl;
+    cout << "eq2:  " << solver.addConstraintNoException(eq2) << "\t" << eq2 << endl;
+    cerr << solver << endl;
+    cout << "eq23: " << solver.addConstraintNoException(eq23) << "\t" << eq23 << endl;
+    cerr << solver << endl;
+    cout << "eq25: " << solver.addConstraintNoException(eq25) << "\t" << eq25 << endl;
+    cerr << solver << endl;
+    cout << "eq27: " << solver.addConstraintNoException(eq27) << "\t" << eq27 << endl;
+    cerr << solver << endl;
+
+    cout << "-----" << endl;
+    solver.solve();
+    cerr << solver << endl;
+    cout << "-----" << endl;
+
+    cout << "eq27: " << solver.removeConstraintNoException(eq27) << "\t" << eq27 << endl;
+    cerr << solver << endl;
+    cout << "eq25: " << solver.removeConstraintNoException(eq25) << "\t" << eq25 << endl;
+    cerr << solver << endl;
+    cout << "eq23: " << solver.removeConstraintNoException(eq23) << "\t" << eq23 << endl;
+    cerr << solver << endl;
+    cout << "eq2:  " << solver.removeConstraintNoException(eq2) << "\t" << eq2 << endl;
+    cerr << solver << endl;
+    cout << "eq26: " << solver.removeConstraintNoException(eq26) << "\t" << eq26 << endl;
+    cerr << solver << endl;
+    cout << "eq12: " << solver.removeConstraintNoException(eq12) << "\t" << eq12 << endl;
+    cerr << solver << endl;
+    cout << "eq13: " << solver.removeConstraintNoException(eq13) << "\t" << eq13 << endl;
+    cerr << solver << endl;
+    cout << "eq14: " << solver.removeConstraintNoException(eq14) << "\t" << eq14 << endl;
+    cerr << solver << endl;
+    cout << "eq15: " << solver.removeConstraintNoException(eq15) << "\t" << eq15 << endl;
+    cerr << solver << endl;
+    cout << "eq16: " << solver.removeConstraintNoException(eq16) << "\t" << eq16 << endl;
+    cerr << solver << endl;
+    cout << "eq17: " << solver.removeConstraintNoException(eq17) << "\t" << eq17 << endl;
+    cerr << solver << endl;
+    cout << "eq24: " << solver.removeConstraintNoException(eq24) << "\t" << eq24 << endl;
+    cerr << solver << endl;
+    cout << "eq29: " << solver.removeConstraintNoException(eq29) << "\t" << eq29 << endl;
+    cerr << solver << endl;
+    cout << "eq1:  " << solver.removeConstraintNoException(eq1) << "\t" << eq1 << endl;
+    cerr << solver << endl;
+
+    solver.solve();
+
+    cout << r1 << " = " << r1.value() << endl
+         << r4 << " = " << r4.value() << endl
+         << r5 << " = " << r5.value() << endl;
+#endif
+
+#if 1
+    cout << "eq1:  " << solver.addConstraintNoException(eq1) << "\t" << eq1 << endl << solver << endl;
+    cout << "eq17:  " << solver.addConstraintNoException(eq17) << "\t" << eq17 << endl << solver << endl;
+    cout << "eq16:  " << solver.addConstraintNoException(eq16) << "\t" << eq16 << endl << solver << endl;
+    cout << "eq15:  " << solver.addConstraintNoException(eq15) << "\t" << eq15 << endl << solver << endl;
+    cout << "eq14:  " << solver.addConstraintNoException(eq14) << "\t" << eq14 << endl << solver << endl;
+    cout << "eq13:  " << solver.addConstraintNoException(eq13) << "\t" << eq13 << endl << solver << endl;
+    cout << "eq12:  " << solver.addConstraintNoException(eq12) << "\t" << eq12 << endl << solver << endl;
+    cout << "eq2:  " << solver.addConstraintNoException(eq2) << "\t" << eq2 << endl << solver << endl;
+    cout << "eq29:  " << solver.addConstraintNoException(eq29) << "\t" << eq29 << endl << solver << endl;
+    cout << "eq27:  " << solver.addConstraintNoException(eq27) << "\t" << eq27 << endl << solver << endl;
+    cout << "eq25:  " << solver.addConstraintNoException(eq25) << "\t" << eq25 << endl << solver << endl;
+    cout << "REMeq25:  " << solver.removeConstraintNoException(eq25) << "\t" << eq25 << endl << solver << endl;
+    cout << "REMeq27:  " << solver.removeConstraintNoException(eq27) << "\t" << eq27 << endl << solver << endl;
+    cout << "REMeq29:  " << solver.removeConstraintNoException(eq29) << "\t" << eq29 << endl << solver << endl;
+    cout << "eq23:  " << solver.addConstraintNoException(eq23) << "\t" << eq23 << endl << solver << endl;
+    cout << "eq23:  " << solver.addConstraintNoException(eq23) << "\t" << eq23 << endl << solver << endl;
+    cout << "eq29:  " << solver.addConstraintNoException(eq29) << "\t" << eq29 << endl << solver << endl;
+    cout << "eq27:  " << solver.addConstraintNoException(eq27) << "\t" << eq27 << endl << solver << endl;
+    cout << "eq25:  " << solver.addConstraintNoException(eq25) << "\t" << eq25 << endl << solver << endl;
+    cout << "eq24:  " << solver.addConstraintNoException(eq24) << "\t" << eq24 << endl << solver << endl;
+    cout << "eq24:  " << solver.addConstraintNoException(eq24) << "\t" << eq24 << endl << solver << endl;
+    cout << "eq18:  " << solver.addConstraintNoException(eq18) << "\t" << eq18 << endl << solver << endl;
+    cout << "eq22:  " << solver.addConstraintNoException(eq22) << "\t" << eq22 << endl << solver << endl;
+    cout << "eq20:  " << solver.addConstraintNoException(eq20) << "\t" << eq20 << endl << solver << endl;
+    cout << "eq26:  " << solver.addConstraintNoException(eq26) << "\t" << eq26 << endl << solver << endl;
+    cout << "eq21:  " << solver.addConstraintNoException(eq21) << "\t" << eq21 << endl << solver << endl;
+    cout << "eq19:  " << solver.addConstraintNoException(eq19) << "\t" << eq19 << endl << solver << endl;
+    cout << "eq28:  " << solver.addConstraintNoException(eq28) << "\t" << eq28 << endl << solver << endl;
+    cout << "eq23:  " << solver.addConstraintNoException(eq23) << "\t" << eq23 << endl << solver << endl;
+    cout << "eq24:  " << solver.addConstraintNoException(eq24) << "\t" << eq24 << endl << solver << endl;
+    
+
+    cout << r1 << " = " << r1.value() << endl
+         << r2 << " = " << r2.value() << endl
+         << r3 << " = " << r3.value() << endl
+         << r4 << " = " << r4.value() << endl
+         << r5 << " = " << r5.value() << endl
+         << r6 << " = " << r6.value() << endl
+         << r7 << " = " << r7.value() << endl
+         << r8 << " = " << r8.value() << endl;
+
+#endif
+
+    return false;
+    }
+  catch (ExCLError &error)
+    {
+   cerr << "Exception! " << error.description() << endl;
+   return(true);
    }
  catch (...) 
    {
@@ -418,12 +624,13 @@ main( int argc, char **argv )
     fResult = x(); fAllOkResult &= fResult; \
     if (!fResult) cout << "Failed!" << endl;
     
-    RUN_TEST(justStay1);
-    RUN_TEST(addDelete1);
-    RUN_TEST(addDelete2);
-    RUN_TEST(casso1);
-    RUN_TEST(inconsistent1);
-    RUN_TEST(inconsistent2);
+    //    RUN_TEST(justStay1);
+    //    RUN_TEST(addDelete1);
+    //    RUN_TEST(addDelete2);
+    //    RUN_TEST(casso1);
+    //    RUN_TEST(inconsistent1);
+    //    RUN_TEST(inconsistent2);
+    RUN_TEST(inconsistent3);
 
     int cns = 900, vars = 900, resolves = 10000;
 
