4d161443ed5e909b5cd6ace9e1f3bc59661a9e2c
gjb <>
1998-07-24 16:05:25 +0000

    Permit string as arg to ctr for ExCLInternalError and ExCLBadResolve so more descriptive description() can be given.

--- c++/ClErrors.h
+++ c++/ClErrors.h
@@ -13,30 +13,49 @@
 #define ClErrors_H
 
 #include "Cassowary.h"
+#include <string.h>
 #include <stdexcept>
 
 class ExCLError : public exception {
  public:
+  ExCLError() : _msg(NULL) { }
   virtual char *description() const
     { return "(ExCLError) An error has occured in CL"; }
+ protected:
+  char *_msg;
 };
 
 class ExCLInternalError : public ExCLError {
  public:
+  ExCLInternalError(const char *sz)
+    { _msg = strdup(sz); }
   virtual char *description() const
-    { return "(ExCLInternalError) An internal error has occurred"; }
+    { 
+      if (_msg) return _msg;
+      else return "(ExCLInternalError) An internal error has occurred"; 
+    }
 };
 
 class ExCLBadResolve : public ExCLError {
  public:
+  ExCLBadResolve(const char *sz)
+    { _msg = strdup(sz); }
   virtual char *description() const
-    { return "(ExCLBadResolve) Number of resolve values did not match number of edit vars"; }
+    {
+      if (_msg) return _msg;
+      else return "(ExCLBadResolve) Number of resolve values did not match number of edit vars"; 
+    }
 };
 
 class ExCLEditMisuse : public ExCLError {
  public:
+  ExCLEditMisuse(const char *sz)
+    { _msg = strdup(sz); }
   virtual char *description() const
-    { return "(ExCLEditMisuse) Edit protocol usage violation"; }
+    {
+      if (_msg) return _msg;
+      return "(ExCLEditMisuse) Edit protocol usage violation"; 
+    }
 };
 
 
