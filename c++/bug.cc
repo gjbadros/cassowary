a5a507b98eba68c53d12219d7fdd38bee941ad55
gjb <>
1998-09-09 16:47:29 +0000

    Use typename, so only illustrate real bug in egcs-1.1b

--- c++/bug.cc
+++ c++/bug.cc
@@ -1,6 +1,6 @@
 #include <map>
 
-/* compile w/ egcs-1.1b, -pedantic and/or -DCRASH */
+/* compile w/ egcs-1.1b and -DCRASH */
 
 #ifndef CRASH
 class ClAbstractVariable;
@@ -14,6 +14,6 @@ class ClGenericLinearExpression  {
 
   T coefficientFor(const ClAbstractVariable &var) const
     { 
-    ClVarToCoeffMap::const_iterator it = _terms.find(&var);
+    typename ClVarToCoeffMap::const_iterator it = _terms.find(&var);
     }
 };
