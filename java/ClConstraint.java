1f65605d07851f17abbc33e36c9f28cc5ac4888c
gjb <>
1999-02-16 01:48:33 +0000

    Updated copyright

--- java/ClConstraint.java
+++ java/ClConstraint.java
@@ -4,7 +4,7 @@
 // Original Smalltalk Implementation by Alan Borning
 // This Java Implementation by Greg J. Badros, <gjb@cs.washington.edu>
 // http://www.cs.washington.edu/homes/gjb
-// (C) 1998 Alan Borning and Greg Badros
+// (C) 1998, 1999 Alan Borning and Greg Badros
 // See ../LICENSE for legal details regarding this software
 //
 // ClConstraint
