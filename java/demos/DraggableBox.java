4d880d05a38993c6c1ab119f1c82a66bda07127b
gjb <>
1999-03-16 22:15:02 +0000

    Fix copyright notices, initial comment

--- java/demos/DraggableBox.java
+++ java/demos/DraggableBox.java
@@ -4,14 +4,8 @@
 // Original Smalltalk Implementation by Alan Borning
 // This C++ Implementation by Greg J. Badros, <gjb@cs.washington.edu>
 // http://www.cs.washington.edu/homes/gjb
-// (C) 1998, All Rights Reserved.
-//
-// (c) 1998 Alan Borning, Michael Noth, and Greg Badros.  
-// This code is provided for use by students for course projects in
-// the course CSE 595 in the Department of Computer Science and
-// Engineering, University of Washington, during winter quarter 1998.
-// Any other use requires written permission from the copyright
-// holders.
+// (c) 1998, 1999 Alan Borning, Michael Noth, and Greg Badros.  
+// See ../../LICENSE for legal details regarding this software
 //
 // Implementation of the DraggableBox class
 // By Michael Noth, 9 Feb 1998
