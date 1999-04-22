6bff5f3f7c12779d70bd2d7cd6b647aa14ba9a71
gjb <>
1999-04-22 09:13:38 +0000

    Added

---
+++ c++/ClFDVariable.cc
@@ -0,0 +1,32 @@
+// $Id$
+//
+// Cassowary Incremental Constraint Solver
+// Original Smalltalk Implementation by Alan Borning
+// This C++ Implementation by Greg J. Badros, <gjb@cs.washington.edu>
+// http://www.cs.washington.edu/homes/gjb
+// (C) 1998, 1999 Greg J. Badros and Alan Borning
+// See ../LICENSE for legal details regarding this software
+//
+// ClFDVariable.cc
+
+#include "ClFDVariable.h"
+
+#ifdef HAVE_CONFIG_H
+#include <cassowary/config.h>
+#define CONFIG_H_INCLUDED
+#endif
+
+#ifndef CL_NO_IO
+ostream &ClFDVariable::PrintOn(ostream &xo) const
+{  
+  xo << "[" << Name() << ":" << _value << "]";
+  return xo;
+}
+#endif
+
+
+void ClFDVariable::SetName(string const &name)
+{ 
+  super::SetName(name); 
+}
+
